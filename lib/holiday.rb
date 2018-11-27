a868c13813027d80cde1f856b948eb306b3ae186
Drake Rentrop <drakerentrop@gmail.com>
2018-11-27 23:01:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
-      supplies = supplies.join(" , ")
-      holiday = holiday.to_s.split("_").map {|w| w.capitalize}
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
       puts "  " + holiday + " " + supplies
     end
   end
